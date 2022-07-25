63238072dace4c555e1e8587e115488a825fa692
Giovanni Marcatto <gzmarcatto@gmail.com>
2022-07-25 11:12:44 -0300

    v22.07.25 layout menu

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./assets/css/reset.css";
+import "./assets/css/style.css";
+import Start from "./Start";
+
+export default function App(){
+  return(
+    <Start />
+  )
+}
---
+++ src/Start.js
@@ -0,0 +1,9 @@
+export default function Start() {
+  return (
+    <div class="menu">
+    <img src="../src/assets/img/logo.png" alt="Logo" />
+    <div class="title"><span>ZapRecall</span></div>
+    <div class="start">Iniciar Recall!</div>
+  </div>
+  );
+}
